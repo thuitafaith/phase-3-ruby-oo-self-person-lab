9eb2830b3ffff2f5d1308391e0efb5b99264743c
thuitaf <faith.thuita@moringaschool.com>
2022-09-08 12:35:36 +0300

    commit tree c1889f789624c27691d60da222e12ea23859d76c

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,37 @@
 # your code goes here
+class Person
+
+    attr_accessor :bank_account
+    attr_reader :name, :happiness, :hygiene
+
+  def initialize(name)
+    @name = name
+    @bank_account = 25
+    @happiness = 8
+    @hygiene = 8
+  end
+
+  def happiness=(num)
+    @happiness =  if num > 10
+                    10
+                  elsif num < 0
+                    0
+                  else
+                    num
+                  end
+
+
+  end
+
+  def hygiene=(num)
+    @hygiene =  if num > 10
+                  10
+                elsif num < 0
+                  0
+                else
+                  num
+                end
+    
+  end
+ 
+end
